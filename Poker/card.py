b47168a5991db367ddf6089ed6339796b9f8deed
hype258 <karol.ogonowski@poczta.onet.eu>
2025-04-30 16:11:35 +0200

    poker 0.2v
    dodana zostala logika gry(ocena układu, również z kolorem), zastąpione zostały nazwy kolorów znaczkami, dodanie funkcji wymiany kart, oraz prostego obstawiania

--- Poker/card.py
+++ Poker/card.py
@@ -1,12 +1,20 @@
 class Card:
     suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+    suit_symbols = {
+        'Clubs': '\u2663',
+        'Diamonds': '\u2666',
+        'Hearts': '\u2665',
+        'Spades': '\u2660'
+    }
+
     def __init__(self, rank, suit):
         self.suit = suit
         self.rank = rank
 
     def __str__(self):
-        return f"{self.rank}_{self.suit}"
+        return f"{self.rank}{self.suit_symbols[self.suit]}"
 
     def __repr__(self):
         return self.__str__()
--- Poker/test.py
+++ Poker/test.py
@@ -1,3 +1,4 @@
+from Poker.game_logic import hand_rank
 from deck import Deck
 from player import Player
 
@@ -12,3 +13,9 @@ player2.receive_cards(deck.deal(5))
 
 print(player1.name, ":", player1.show_hand())
 print(player2.name, ":", player2.show_hand())
+print(player1.hand_rank())
+print(player2.hand_rank())
+player1.exchange_cards([3, 4], deck)
+print("Po wymianie:")
+print(player1.show_hand())
+print(player1.hand_rank())
